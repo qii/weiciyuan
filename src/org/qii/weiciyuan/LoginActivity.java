61f1a3c095a9fff4eafb3c6f99d99e10ea672834
qii <qiibeta@gmail.com>
2012-07-27 13:16:24 +0800

    test

--- src/org/qii/weiciyuan/LoginActivity.java
+++ src/org/qii/weiciyuan/LoginActivity.java
@@ -1,7 +1,11 @@
 package org.qii.weiciyuan;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import org.qii.weiciyuan.ui.HomeActivity;
 
 public class LoginActivity extends Activity {
     /**
@@ -11,6 +15,15 @@ public class LoginActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        SharedPreferences settings = getPreferences(MODE_PRIVATE);
+        String account = settings.getString("username", "");
+        if (!TextUtils.isEmpty(account)) {
+            startActivity(new Intent(LoginActivity.this, HomeActivity.class));
+            finish();
+        }
+
+
 
     }
 }
