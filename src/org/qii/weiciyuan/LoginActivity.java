899d035b7ca42760b30b531367e77bca13b68e2f
qii <qiibeta@gmail.com>
2012-07-27 11:22:29 +0800

    1

---
+++ src/org/qii/weiciyuan/LoginActivity.java
@@ -0,0 +1,15 @@
+package org.qii.weiciyuan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LoginActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
