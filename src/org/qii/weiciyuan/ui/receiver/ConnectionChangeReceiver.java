45b6a41dd7e0d568cde8f6b7e85ea7ba7cef009d
qii <qiibeta@gmail.com>
2012-08-06 16:58:25 +0800

    receiver

---
+++ src/org/qii/weiciyuan/ui/receiver/ConnectionChangeReceiver.java
@@ -0,0 +1,27 @@
+package org.qii.weiciyuan.ui.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * User: Jiang Qi
+ * Date: 12-8-6
+ * Time: 下午4:38
+ */
+public class ConnectionChangeReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        ConnectivityManager cm = (ConnectivityManager)
+                context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+
+
+        }
+
+    }
+}
--- src/org/qii/weiciyuan/ui/receiver/MentionsAndCommentsReceiver.java
+++ src/org/qii/weiciyuan/ui/receiver/MentionsAndCommentsReceiver.java
@@ -1,17 +1,19 @@
-package org.qii.weiciyuan.ui.timeline;
+package org.qii.weiciyuan.ui.receiver;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 /**
  * User: Jiang Qi
  * Date: 12-7-31
  * Time: 上午9:05
  */
-public class NewMsgNotificationBroadcastReceiver extends BroadcastReceiver {
+public class MentionsAndCommentsReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
+
 
     }
 }
