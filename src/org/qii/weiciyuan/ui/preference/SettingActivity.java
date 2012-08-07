c48af37f6fd2f2b6832606cea7108828431d349f
qii <qiibeta@gmail.com>
2012-08-07 17:15:39 +0800

    settings

--- src/org/qii/weiciyuan/ui/preference/SettingActivity.java
+++ src/org/qii/weiciyuan/ui/preference/SettingActivity.java
@@ -1,7 +1,9 @@
 package org.qii.weiciyuan.ui.preference;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 import android.view.MenuItem;
 import org.qii.weiciyuan.R;
@@ -16,6 +18,7 @@ public class SettingActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setTitle("Settings");
 
         getFragmentManager().beginTransaction()
                 .replace(android.R.id.content, new SettingsFragment())
@@ -32,13 +35,38 @@ public class SettingActivity extends Activity {
                 return super.onOptionsItemSelected(item);
         }
     }
+
 }
 
-class SettingsFragment extends PreferenceFragment {
+class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.pref);
+
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+
+        findPreference("clear_cache").setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                return false;  //To change body of implemented methods use File | Settings | File Templates.
+            }
+        });
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        getPreferenceScreen().getSharedPreferences()
+                .unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+//        Toast.makeText(getActivity(), "clgggggear", Toast.LENGTH_SHORT).show();
+//        if (key.equals("clear_cache")) {
+//            Toast.makeText(getActivity(), "clear", Toast.LENGTH_SHORT).show();
+//        }
     }
 }
