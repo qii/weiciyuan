dd9332a036efce7662bf00c96273a4691b52f4b1
qii <qiibeta@gmail.com>
2012-09-06 19:08:40 +0800

    fix bug

--- src/org/qii/weiciyuan/ui/preference/SettingsFragment.java
+++ src/org/qii/weiciyuan/ui/preference/SettingsFragment.java
@@ -127,11 +127,12 @@ public class SettingsFragment extends PreferenceFragment implements SharedPrefer
 
         Intent intent = getActivity().getIntent();
         getActivity().overridePendingTransition(0, 0);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
         getActivity().finish();
 
         getActivity().overridePendingTransition(0, 0);
         startActivity(intent);
+        getActivity().overridePendingTransition(R.anim.stay,R.anim.alphaout);
     }
 
 
