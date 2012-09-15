9612472eeaefa30da023e325d962e3fe864c41dc
qii <qiibeta@gmail.com>
2012-09-15 12:02:12 +0800

    error

--- src/org/qii/weiciyuan/support/utils/AppConfig.java
+++ src/org/qii/weiciyuan/support/utils/AppConfig.java
@@ -3,8 +3,7 @@ package org.qii.weiciyuan.support.utils;
 /**
  * User: Jiang Qi
  * Date: 12-8-1
- * Time: 上午10:28
- */
+  */
 public class AppConfig {
     public static final int DEFAULT_MSG_NUMBERS=50;
 }
