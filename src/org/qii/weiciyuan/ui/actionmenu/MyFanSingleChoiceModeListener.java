970dde07e397a22cb44f75a56a161a39a5535ce8
qii <qiibeta@gmail.com>
2012-10-12 11:23:09 +0800

    fix

---
+++ src/org/qii/weiciyuan/ui/actionmenu/MyFanSingleChoiceModeListener.java
@@ -0,0 +1,270 @@
+package org.qii.weiciyuan.ui.actionmenu;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.text.TextUtils;
+import android.view.ActionMode;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.Toast;
+import org.qii.weiciyuan.R;
+import org.qii.weiciyuan.bean.UserBean;
+import org.qii.weiciyuan.dao.relationship.FanDao;
+import org.qii.weiciyuan.dao.relationship.FriendshipsDao;
+import org.qii.weiciyuan.support.error.WeiboException;
+import org.qii.weiciyuan.support.lib.MyAsyncTask;
+import org.qii.weiciyuan.support.utils.AppLogger;
+import org.qii.weiciyuan.support.utils.GlobalContext;
+import org.qii.weiciyuan.ui.Abstract.IToken;
+import org.qii.weiciyuan.ui.adapter.UserListAdapter;
+import org.qii.weiciyuan.ui.basefragment.AbstractUserListFragment;
+import org.qii.weiciyuan.ui.send.StatusNewActivity;
+
+/**
+ * User: qii
+ * Date: 12-10-9
+ */
+public class MyFanSingleChoiceModeListener implements ActionMode.Callback {
+    private ListView listView;
+    private UserListAdapter adapter;
+    private Fragment fragment;
+    private ActionMode mode;
+    private UserBean bean;
+
+
+    private MyAsyncTask<Void, UserBean, UserBean> followOrUnfollowTask;
+
+
+    public void finish() {
+        if (mode != null)
+            mode.finish();
+
+        if (followOrUnfollowTask != null)
+            followOrUnfollowTask.cancel(true);
+    }
+
+    public MyFanSingleChoiceModeListener(ListView listView, UserListAdapter adapter, Fragment fragment, UserBean bean) {
+        this.listView = listView;
+        this.fragment = fragment;
+        this.adapter = adapter;
+        this.bean = bean;
+    }
+
+    private Activity getActivity() {
+        return fragment.getActivity();
+    }
+
+
+    @Override
+    public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+        if (this.mode == null)
+            this.mode = mode;
+
+        return true;
+
+    }
+
+    @Override
+    public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+        MenuInflater inflater = mode.getMenuInflater();
+        menu.clear();
+
+        inflater.inflate(R.menu.contextual_menu_myfansinglechoicemodelistener, menu);
+
+        mode.setTitle(bean.getScreen_name());
+
+
+        return true;
+
+
+    }
+
+    @Override
+    public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menu_at:
+                Intent intent = new Intent(getActivity(), StatusNewActivity.class);
+                intent.putExtra("token", ((IToken) getActivity()).getToken());
+                intent.putExtra("content", "@" + bean.getScreen_name());
+                intent.putExtra("accountName", GlobalContext.getInstance().getCurrentAccountName());
+                intent.putExtra("accountId", GlobalContext.getInstance().getCurrentAccountId());
+                getActivity().startActivity(intent);
+                listView.clearChoices();
+                mode.finish();
+                break;
+            case R.id.menu_follow:
+                if (followOrUnfollowTask == null || followOrUnfollowTask.getStatus() == MyAsyncTask.Status.FINISHED) {
+                    followOrUnfollowTask = new FollowTask();
+                    followOrUnfollowTask.executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
+                }
+                listView.clearChoices();
+                mode.finish();
+                break;
+            case R.id.menu_unfollow:
+                if (followOrUnfollowTask == null || followOrUnfollowTask.getStatus() == MyAsyncTask.Status.FINISHED) {
+                    followOrUnfollowTask = new UnFollowTask();
+                    followOrUnfollowTask.executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
+                }
+                listView.clearChoices();
+                mode.finish();
+                break;
+            case R.id.menu_remove_fan:
+                if (followOrUnfollowTask == null || followOrUnfollowTask.getStatus() == MyAsyncTask.Status.FINISHED) {
+                    followOrUnfollowTask = new RemoveFanTask();
+                    followOrUnfollowTask.execute();
+                }
+                listView.clearChoices();
+                mode.finish();
+                break;
+        }
+
+
+        return true;
+    }
+
+
+    private class FollowTask extends MyAsyncTask<Void, UserBean, UserBean> {
+        WeiboException e;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected UserBean doInBackground(Void... params) {
+
+            FriendshipsDao dao = new FriendshipsDao(((IToken) getActivity()).getToken());
+            if (!TextUtils.isEmpty(bean.getId())) {
+                dao.setUid(bean.getId());
+            } else {
+                dao.setScreen_name(bean.getScreen_name());
+            }
+            try {
+                return dao.followIt();
+            } catch (WeiboException e) {
+                AppLogger.e(e.getError());
+                this.e = e;
+                cancel(true);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onCancelled(UserBean userBean) {
+            super.onCancelled(userBean);
+            if (e != null) {
+                Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(UserBean o) {
+            super.onPostExecute(o);
+            Toast.makeText(getActivity(), getActivity().getString(R.string.follow_successfully), Toast.LENGTH_SHORT).show();
+            adapter.update(bean, o);
+        }
+    }
+
+    private class UnFollowTask extends MyAsyncTask<Void, UserBean, UserBean> {
+        WeiboException e;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected UserBean doInBackground(Void... params) {
+
+            FriendshipsDao dao = new FriendshipsDao(((IToken) getActivity()).getToken());
+            if (!TextUtils.isEmpty(bean.getId())) {
+                dao.setUid(bean.getId());
+            } else {
+                dao.setScreen_name(bean.getScreen_name());
+            }
+
+            try {
+                return dao.unFollowIt();
+            } catch (WeiboException e) {
+                AppLogger.e(e.getError());
+                this.e = e;
+                cancel(true);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onCancelled(UserBean userBean) {
+            super.onCancelled(userBean);
+            if (e != null) {
+                Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(UserBean o) {
+            super.onPostExecute(o);
+            Toast.makeText(getActivity(), getActivity().getString(R.string.unfollow_successfully), Toast.LENGTH_SHORT).show();
+            adapter.update(bean, o);
+        }
+    }
+
+
+    private class RemoveFanTask extends MyAsyncTask<Void, UserBean, UserBean> {
+        WeiboException e;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected UserBean doInBackground(Void... params) {
+
+            FanDao dao = new FanDao(((IToken) getActivity()).getToken(), bean.getId());
+
+            try {
+                return dao.removeFan();
+            } catch (WeiboException e) {
+                AppLogger.e(e.getError());
+                this.e = e;
+                cancel(true);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onCancelled(UserBean userBean) {
+            super.onCancelled(userBean);
+            if (e != null) {
+                Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(UserBean o) {
+            super.onPostExecute(o);
+            Toast.makeText(getActivity(), getActivity().getString(R.string.remove_fan_successfully), Toast.LENGTH_SHORT).show();
+            adapter.removeItem(bean);
+        }
+    }
+
+
+    @Override
+    public void onDestroyActionMode(ActionMode mode) {
+        this.mode = null;
+        listView.clearChoices();
+        adapter.notifyDataSetChanged();
+        ((AbstractUserListFragment) fragment).setmActionMode(null);
+
+    }
+
+}
+
