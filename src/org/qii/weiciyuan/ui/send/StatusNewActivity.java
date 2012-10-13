a805a8674beca77ae69bba71d36df23bc33601c7
qii <qiibeta@gmail.com>
2012-10-13 19:42:34 +0800

    fix

--- src/org/qii/weiciyuan/ui/send/StatusNewActivity.java
+++ src/org/qii/weiciyuan/ui/send/StatusNewActivity.java
@@ -5,6 +5,7 @@ import android.content.*;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.AssetManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.location.*;
@@ -353,7 +354,17 @@ public class StatusNewActivity extends AbstractAppActivity implements DialogInte
     }
 
     private String getPicPathFromUri(Uri uri) {
-        return uri.getPath();
+        String value = uri.getPath();
+
+        if (value.startsWith("/external")) {
+            String[] proj = {MediaStore.Images.Media.DATA};
+            Cursor cursor = managedQuery(uri, proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } else {
+            return value;
+        }
     }
 
     private boolean canSend() {
