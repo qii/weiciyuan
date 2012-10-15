813322f59f0608014b9aa9ee10cce9b46b4bb216
qii <qiibeta@gmail.com>
2012-10-15 22:57:54 +0800

    refactor

--- src/org/qii/weiciyuan/support/utils/TimeTool.java
+++ src/org/qii/weiciyuan/support/utils/TimeTool.java
@@ -16,6 +16,13 @@ public class TimeTool {
     private static int MILL_HOUR = MILL_MIN * 60;
     private static int MILL_DAY = MILL_HOUR * 24;
 
+    private static String JUST_NOW = GlobalContext.getInstance().getString(R.string.justnow);
+    private static String MIN = GlobalContext.getInstance().getString(R.string.min);
+    private static String HOUR = GlobalContext.getInstance().getString(R.string.hour);
+    private static String DAY = GlobalContext.getInstance().getString(R.string.day);
+    private static String MONTH = GlobalContext.getInstance().getString(R.string.month);
+    private static String YEAR = GlobalContext.getInstance().getString(R.string.year);
+
     public static String getListTime(ItemBean bean) {
         long now = System.currentTimeMillis();
         long msg = 0L;
@@ -32,36 +39,36 @@ public class TimeTool {
         long calSeconds = calcMills / 1000;
 
         if (calSeconds < 60) {
-            return GlobalContext.getInstance().getString(R.string.justnow);
+            return JUST_NOW;
         }
 
         long calMins = calSeconds / 60;
 
         if (calMins < 60) {
 
-            return "" + calMins + GlobalContext.getInstance().getString(R.string.min);
+            return new StringBuilder().append(calMins).append(MIN).toString();
         }
 
         long calHours = calMins / 60;
 
         if (calHours < 24) {
-            return "" + calHours + GlobalContext.getInstance().getString(R.string.hour);
+            return new StringBuilder().append(calHours).append(HOUR).toString();
         }
 
         long calDay = calHours / 24;
 
         if (calDay < 31) {
-            return "" + calDay + GlobalContext.getInstance().getString(R.string.day);
+            return new StringBuilder().append(calDay).append(DAY).toString();
         }
 
         long calMonth = calDay / 31;
 
         if (calMonth < 12) {
-            return "" + calMonth + GlobalContext.getInstance().getString(R.string.month);
+            return new StringBuilder().append(calMonth).append(MONTH).toString();
 
         }
 
-        return "" + calMonth / 12 + GlobalContext.getInstance().getString(R.string.year);
+        return new StringBuilder().append(calMonth / 12).append(YEAR).toString();
 
     }
 
