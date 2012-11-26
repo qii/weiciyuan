0d4649051dbe9972523a9114c789c70f6cc428bd
qii <qiibeta@gmail.com>
2012-11-26 21:01:28 +0800

    add  topic

---
+++ src/org/qii/weiciyuan/ui/topic/FollowTopicDialog.java
@@ -0,0 +1,53 @@
+package org.qii.weiciyuan.ui.topic;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.WindowManager;
+import android.widget.EditText;
+import org.qii.weiciyuan.R;
+
+/**
+ * User: qii
+ * Date: 12-11-26
+ */
+public class FollowTopicDialog extends DialogFragment {
+
+
+    public FollowTopicDialog() {
+
+    }
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final EditText et = new EditText(getActivity());
+        builder.setView(et)
+                .setTitle(getString(R.string.add_topic))
+                .setPositiveButton(getString(R.string.add), new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String word = et.getText().toString().trim();
+                        if (!TextUtils.isEmpty(word)) {
+                            UserTopicListFragment userTopicListFragment = (UserTopicListFragment) getTargetFragment();
+                            userTopicListFragment.addTopic(word);
+                        }
+                    }
+                })
+                .setNegativeButton(getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        AlertDialog dialog = builder.create();
+        dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        return dialog;
+    }
+}
--- src/org/qii/weiciyuan/ui/topic/UserTopicListFragment.java
+++ src/org/qii/weiciyuan/ui/topic/UserTopicListFragment.java
@@ -3,10 +3,16 @@ package org.qii.weiciyuan.ui.topic;
 import android.app.ListFragment;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Toast;
+import org.qii.weiciyuan.R;
 import org.qii.weiciyuan.bean.UserBean;
+import org.qii.weiciyuan.dao.topic.TopicDao;
 import org.qii.weiciyuan.dao.topic.UserTopicListDao;
 import org.qii.weiciyuan.support.error.WeiboException;
 import org.qii.weiciyuan.support.lib.MyAsyncTask;
@@ -30,14 +36,16 @@ public class UserTopicListFragment extends ListFragment {
 
     private TopicListTask task;
 
+    private FollowTopicTask followTopicTask;
+
 
     public UserTopicListFragment() {
 
     }
 
     public UserTopicListFragment(UserBean userBean) {
-          this.userBean = userBean;
-      }
+        this.userBean = userBean;
+    }
 
 
     public UserTopicListFragment(UserBean userBean, ArrayList<String> topicList) {
@@ -49,7 +57,7 @@ public class UserTopicListFragment extends ListFragment {
     @Override
     public void onDetach() {
         super.onDetach();
-        Utility.cancelTasks(task);
+        Utility.cancelTasks(task, followTopicTask);
     }
 
 
@@ -97,6 +105,26 @@ public class UserTopicListFragment extends ListFragment {
         }
     }
 
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        if (userBean.getId().equals(GlobalContext.getInstance().getCurrentAccountId()))
+            inflater.inflate(R.menu.actionbar_menu_usertopiclistfragment, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_add_topic:
+                FollowTopicDialog dialog = new FollowTopicDialog();
+                dialog.setTargetFragment(this, 1);
+                dialog.show(getFragmentManager(), "");
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     class TopicListTask extends MyAsyncTask<Void, List<String>, List<String>> {
         WeiboException e;
@@ -127,4 +155,51 @@ public class UserTopicListFragment extends ListFragment {
             adapter.notifyDataSetChanged();
         }
     }
+
+    public void addTopic(String keyWord) {
+        if (Utility.isTaskStopped(followTopicTask)) {
+            followTopicTask = new FollowTopicTask(keyWord);
+            followTopicTask.executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
+        }
+    }
+
+    private class FollowTopicTask extends MyAsyncTask<Void, Boolean, Boolean> {
+        WeiboException e;
+        String keyWord;
+
+        public FollowTopicTask(String keyWord) {
+            this.keyWord = keyWord;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            try {
+                return new TopicDao(GlobalContext.getInstance().getSpecialToken()).follow(keyWord);
+            } catch (WeiboException e) {
+                this.e = e;
+                cancel(true);
+            }
+            return false;
+        }
+
+        @Override
+        protected void onCancelled(Boolean aBoolean) {
+            super.onCancelled(aBoolean);
+            if (getActivity() != null && e != null) {
+                Toast.makeText(getActivity(), e.getError(), Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            if (getActivity() == null)
+                return;
+            if (aBoolean)
+                Toast.makeText(getActivity(), getString(R.string.follow_topic_successfully), Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(getActivity(), getString(R.string.follow_topic_failed), Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
