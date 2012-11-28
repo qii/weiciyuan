5588ccb79cb1919549cc823191fd6c339af41079
qii <qiibeta@gmail.com>
2012-11-28 23:32:34 +0800

    setting helper

---
+++ src/org/qii/weiciyuan/support/settinghelper/SettingHelper.java
@@ -0,0 +1,58 @@
+package org.qii.weiciyuan.support.settinghelper;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+/**
+ * User: qii
+ * Date: 12-11-28
+ */
+public class SettingHelper {
+    private static SharedPreferences.Editor editor = null;
+    private static SharedPreferences sharedPreferences = null;
+
+    private static SharedPreferences.Editor getEditorObject(Context paramContext) {
+        if (editor == null)
+            editor = PreferenceManager.getDefaultSharedPreferences(paramContext).edit();
+        return editor;
+    }
+
+    public static int getSharedPreferences(Context paramContext, String paramString, int paramInt) {
+        return getSharedPreferencesObject(paramContext).getInt(paramString, paramInt);
+    }
+
+    public static long getSharedPreferences(Context paramContext, String paramString, long paramLong) {
+        return getSharedPreferencesObject(paramContext).getLong(paramString, paramLong);
+    }
+
+    public static Boolean getSharedPreferences(Context paramContext, String paramString, Boolean paramBoolean) {
+        return getSharedPreferencesObject(paramContext).getBoolean(paramString, paramBoolean);
+    }
+
+    public static String getSharedPreferences(Context paramContext, String paramString1, String paramString2) {
+        return getSharedPreferencesObject(paramContext).getString(paramString1, paramString2);
+    }
+
+    private static SharedPreferences getSharedPreferencesObject(Context paramContext) {
+        if (sharedPreferences == null)
+            sharedPreferences = PreferenceManager.getDefaultSharedPreferences(paramContext);
+        return sharedPreferences;
+    }
+
+    public static void setEditor(Context paramContext, String paramString, int paramInt) {
+        getEditorObject(paramContext).putInt(paramString, paramInt).commit();
+    }
+
+    public static void setEditor(Context paramContext, String paramString, long paramLong) {
+        getEditorObject(paramContext).putLong(paramString, paramLong).commit();
+    }
+
+    public static void setEditor(Context paramContext, String paramString, Boolean paramBoolean) {
+        getEditorObject(paramContext).putBoolean(paramString, paramBoolean).commit();
+    }
+
+    public static void setEditor(Context paramContext, String paramString1, String paramString2) {
+        getEditorObject(paramContext).putString(paramString1, paramString2).commit();
+    }
+}
---
+++ src/org/qii/weiciyuan/support/settinghelper/SettingUtility.java
@@ -0,0 +1,57 @@
+package org.qii.weiciyuan.support.settinghelper;
+
+import android.content.Context;
+import org.qii.weiciyuan.support.utils.GlobalContext;
+import org.qii.weiciyuan.ui.preference.SettingActivity;
+
+/**
+ * User: qii
+ * Date: 12-11-28
+ */
+public class SettingUtility {
+
+    private static Context getContext() {
+        return GlobalContext.getInstance();
+    }
+
+    public static boolean isEnableFilter() {
+        return SettingHelper.getSharedPreferences(getContext(), SettingActivity.FILTER, false);
+    }
+
+
+    public static int getFontSize() {
+        return SettingHelper.getSharedPreferences(getContext(), SettingActivity.FONT_SIZE, 15);
+    }
+
+    public static int getAppTheme() {
+        return SettingHelper.getSharedPreferences(getContext(), SettingActivity.THEME, 1);
+    }
+
+
+    public static boolean isEnablePic() {
+        return SettingHelper.getSharedPreferences(getContext(), SettingActivity.DISABLE_DOWNLOAD_AVATAR_PIC, false);
+    }
+
+    public static boolean getEnableBigPic() {
+        return SettingHelper.getSharedPreferences(getContext(), SettingActivity.SHOW_BIG_PIC, false);
+    }
+
+    public static boolean getEnableCommentRepostListAvatar() {
+        return !SettingHelper.getSharedPreferences(getContext(), SettingActivity.CLOSE_COMMENT_AND_REPOST_AVATAR, false);
+    }
+
+
+    public static boolean getEnableAutoRefresh() {
+        return SettingHelper.getSharedPreferences(getContext(), SettingActivity.AUTO_REFRESH, false);
+    }
+
+
+    public static boolean getEnableBigAvatar() {
+        return SettingHelper.getSharedPreferences(getContext(), SettingActivity.SHOW_BIG_AVATAR, false);
+    }
+
+    public Boolean getEnableSound() {
+        return SettingHelper.getSharedPreferences(getContext(), SettingActivity.SOUND, true);
+    }
+
+}
