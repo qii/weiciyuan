90ed0695c1c4310eeebd968157b0708f40c2484b
qii <qiibeta@gmail.com>
2012-12-27 16:56:15 +0800

    fix bug

--- src/org/qii/weiciyuan/support/lib/MyLinkify.java
+++ src/org/qii/weiciyuan/support/lib/MyLinkify.java
@@ -178,7 +178,7 @@ public class MyLinkify {
         ArrayList<LinkSpec> links = new ArrayList<LinkSpec>();
 
         if ((mask & WEB_URLS) != 0) {
-            gatherLinks(links, text, Patterns.WEB_URL,
+            gatherLinks(links, text, MyPatterns.WEB_URL,
                     new String[]{"http://", "https://", "rtsp://"},
                     sUrlMatchFilter, null);
         }
