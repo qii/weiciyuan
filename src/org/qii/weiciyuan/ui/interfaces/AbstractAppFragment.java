8bc1ff0a6b92db30139b6e82a966c67a46f1478e
qii <qiibeta@gmail.com>
2012-12-30 20:07:58 +0800

    abstract

--- src/org/qii/weiciyuan/ui/interfaces/AbstractAppFragment.java
+++ src/org/qii/weiciyuan/ui/interfaces/AbstractAppFragment.java
@@ -8,7 +8,11 @@ import android.os.Bundle;
  * Date: 12-12-30
  */
 public class AbstractAppFragment extends Fragment {
-    private boolean isFirstStartFlag = true;
+    /**
+     * when activity is recycled by system, isFirstTimeStartFlag will be reset to default true,
+     * when activity is recreated because a configuration change for example screen rotate, isFirstTimeStartFlag will stay false
+     */
+    private boolean isFirstTimeStartFlag = true;
 
     protected final int FIRST_TIME_START = 0; //when activity is first time start
     protected final int SCREEN_ROTATE = 1;    //when activity is destroyed and recreated because a configuration change, see setRetainInstance(boolean retain)
@@ -17,16 +21,16 @@ public class AbstractAppFragment extends Fragment {
     protected int getCurrentState(Bundle savedInstanceState) {
 
         if (savedInstanceState != null) {
-            isFirstStartFlag = false;
+            isFirstTimeStartFlag = false;
             return ACTIVITY_DESTROY_AND_CREATE;
         }
 
 
-        if (!isFirstStartFlag) {
+        if (!isFirstTimeStartFlag) {
             return SCREEN_ROTATE;
         }
 
-        isFirstStartFlag = false;
+        isFirstTimeStartFlag = false;
         return FIRST_TIME_START;
     }
 }
