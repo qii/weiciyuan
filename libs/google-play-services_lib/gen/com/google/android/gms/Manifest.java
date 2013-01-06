1517f7b1a9c53a27836121f3580c33189c73bf48
qii <qiibeta@gmail.com>
2013-01-06 13:19:33 +0800

    google map api lib

---
+++ libs/google-play-services_lib/gen/com/google/android/gms/Manifest.java
@@ -0,0 +1,5 @@
+package com.google.android.gms;
+
+/* This stub is for using by IDE only. It is NOT the Manifest class actually packed into APK */
+public final class Manifest {
+}
---
+++ libs/google-play-services_lib/gen/com/google/android/gms/R.java
@@ -0,0 +1,5 @@
+package com.google.android.gms;
+
+/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
+public final class R {
+}
