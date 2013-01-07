5c6ab7babdb956f683efba017ed92555fab8b7a7
qii <qiibeta@gmail.com>
2013-01-07 15:46:02 +0800

    fix

---
+++ src/org/qii/weiciyuan/support/utils/DataMemoryCache.java
@@ -0,0 +1,56 @@
+package org.qii.weiciyuan.support.utils;
+
+import org.qii.weiciyuan.bean.MessageBean;
+import org.qii.weiciyuan.bean.MessageListBean;
+
+import java.util.List;
+
+/**
+ * User: qii
+ * Date: 13-1-6
+ */
+public class DataMemoryCache {
+
+    private static MessageListBean friendTimeLineData = new MessageListBean();
+
+    public static MessageListBean getFriendsTimeLineData() {
+        return friendTimeLineData;
+    }
+
+    public static MessageListBean update(MessageBean msg) {
+
+        updateTimeLineDataCommentCount(msg, msg.getComments_count());
+        updateTimeLineDataRepostCount(msg, msg.getReposts_count());
+        return friendTimeLineData;
+    }
+
+    public static MessageListBean updateTimeLineDataCommentCount(MessageBean msg, int commentCount) {
+        if (msg == null) {
+            return friendTimeLineData;
+        }
+
+        List<MessageBean> msgList = friendTimeLineData.getItemList();
+        for (int i = 0; i < msgList.size(); i++) {
+            if (msgList.get(i).getId().equals(msg.getId())) {
+                msgList.get(i).setComments_count(commentCount);
+                break;
+            }
+        }
+        return friendTimeLineData;
+    }
+
+    public static MessageListBean updateTimeLineDataRepostCount(MessageBean msg, int repostCount) {
+        if (msg == null) {
+            return friendTimeLineData;
+        }
+
+        List<MessageBean> msgList = friendTimeLineData.getItemList();
+        for (int i = 0; i < msgList.size(); i++) {
+            if (msgList.get(i).getId().equals(msg.getId())) {
+                msgList.get(i).setReposts_count(repostCount);
+                break;
+            }
+        }
+        return friendTimeLineData;
+    }
+}
