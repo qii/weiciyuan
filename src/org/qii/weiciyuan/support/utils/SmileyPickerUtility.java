503fdaa42e2ddcd374a4773888eb015602baec33
qii <qiibeta@gmail.com>
2013-01-31 20:38:37 +0800

    utility

---
+++ src/org/qii/weiciyuan/support/utils/SmileyPickerUtility.java
@@ -0,0 +1,67 @@
+package org.qii.weiciyuan.support.utils;
+
+import android.app.Activity;
+import android.graphics.Rect;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+/**
+ * User: qii
+ * Date: 13-1-18
+ */
+public class SmileyPickerUtility {
+    public static void hideSoftInput(EditText paramEditText) {
+        ((InputMethodManager) GlobalContext.getInstance().getSystemService("input_method")).hideSoftInputFromWindow(paramEditText.getWindowToken(), 0);
+    }
+
+    public static void showKeyBoard(final EditText paramEditText) {
+        paramEditText.requestFocus();
+        paramEditText.post(new Runnable() {
+            @Override
+            public void run() {
+                ((InputMethodManager) GlobalContext.getInstance().getSystemService("input_method")).showSoftInput(paramEditText, 0);
+            }
+        });
+    }
+
+
+    public static int getScreenHeight(Activity paramActivity) {
+        Display display = paramActivity.getWindowManager().getDefaultDisplay();
+        DisplayMetrics metrics = new DisplayMetrics();
+        display.getMetrics(metrics);
+        return metrics.heightPixels;
+    }
+
+    public static int getStatusBarHeight(Activity paramActivity) {
+        Rect localRect = new Rect();
+        paramActivity.getWindow().getDecorView().getWindowVisibleDisplayFrame(localRect);
+        return localRect.top;
+
+    }
+
+    public static int getActionBarHeight(Activity paramActivity) {
+        int contentViewTop =
+                paramActivity.getWindow().findViewById(Window.ID_ANDROID_CONTENT).getTop();
+        AppLogger.e("contentViewTop=" + contentViewTop);
+
+        return contentViewTop - getStatusBarHeight(paramActivity);
+    }
+
+    //below status bar,include actionbar, above softkeyboard
+    public static int getAppHeight(Activity paramActivity) {
+        Rect localRect = new Rect();
+        paramActivity.getWindow().getDecorView().getWindowVisibleDisplayFrame(localRect);
+        return localRect.height();
+    }
+
+    public static int getKeyboardHeight(Activity paramActivity) {
+
+        return SmileyPickerUtility.getScreenHeight(paramActivity)
+                - SmileyPickerUtility.getStatusBarHeight(paramActivity)
+                - SmileyPickerUtility.getAppHeight(paramActivity);
+
+    }
+}
