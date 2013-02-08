80cd8a4e61a5d62dc978e2e5d73453e0715498b6
qii <qiibeta@gmail.com>
2013-02-08 12:09:31 +0800

    refactor

--- src/org/qii/weiciyuan/ui/browser/BrowserBigPicActivity.java
+++ src/org/qii/weiciyuan/ui/browser/BrowserBigPicActivity.java
@@ -248,17 +248,16 @@ public class BrowserBigPicActivity extends AbstractAppActivity {
             if (!TextUtils.isEmpty(bitmapPath)) {
                 path = bitmapPath;
 
+                if (ImageTool.isThisBitmapCanRead(bitmapPath)) {
+                    int[] size = ImageTool.getBitmapSize(bitmapPath);
+                    getActionBar().setSubtitle(String.valueOf(size[0]) + "x" + String.valueOf(size[1]));
+                }
+
                 File file = new File(bitmapPath);
 
                 String str1 = "file://" + file.getAbsolutePath().replace("/mnt/sdcard/", "/sdcard/");
                 String str2 = "<html>\n<head>\n     <style>\n          html,body{background:transparent;margin:0;padding:0;}          *{-webkit-tap-highlight-color:rgba(0, 0, 0, 0);}\n     </style>\n     <script type=\"text/javascript\">\n     var imgUrl = \"" + str1 + "\";" + "     var objImage = new Image();\n" + "     var realWidth = 0;\n" + "     var realHeight = 0;\n" + "\n" + "     function onLoad() {\n" + "          objImage.onload = function() {\n" + "               realWidth = objImage.width;\n" + "               realHeight = objImage.height;\n" + "\n" + "               document.gagImg.src = imgUrl;\n" + "               onResize();\n" + "          }\n" + "          objImage.src = imgUrl;\n" + "     }\n" + "\n" + "     function onResize() {\n" + "          var scale = 1;\n" + "          var newWidth = document.gagImg.width;\n" + "          if (realWidth > newWidth) {\n" + "               scale = realWidth / newWidth;\n" + "          } else {\n" + "               scale = newWidth / realWidth;\n" + "          }\n" + "\n" + "          hiddenHeight = Math.ceil(30 * scale);\n" + "          document.getElementById('hiddenBar').style.height = hiddenHeight + \"px\";\n" + "          document.getElementById('hiddenBar').style.marginTop = -hiddenHeight + \"px\";\n" + "     }\n" + "     </script>\n" + "</head>\n" + "<body onload=\"onLoad()\" onresize=\"onResize()\" onclick=\"Android.toggleOverlayDisplay();\">\n" + "     <table style=\"width: 100%;height:100%;\">\n" + "          <tr style=\"width: 100%;\">\n" + "               <td valign=\"middle\" align=\"center\" style=\"width: 100%;\">\n" + "                    <div style=\"display:block\">\n" + "                         <img name=\"gagImg\" src=\"\" width=\"100%\" style=\"\" />\n" + "                    </div>\n" + "                    <div id=\"hiddenBar\" style=\"position:absolute; width: 100%; background: transparent;\"></div>\n" + "               </td>\n" + "          </tr>\n" + "     </table>\n" + "</body>\n" + "</html>";
                 webView.loadDataWithBaseURL("file:///android_asset/", str2, "text/html", "utf-8", null);
-
-
-                if (ImageTool.isThisBitmapCanRead(bitmapPath)) {
-                    int[] size = ImageTool.getBitmapSize(bitmapPath);
-                    getActionBar().setSubtitle(String.valueOf(size[0]) + "x" + String.valueOf(size[1]));
-                }
                 webView.setVisibility(View.VISIBLE);
                 pb.setVisibility(View.INVISIBLE);
 
