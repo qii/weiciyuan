d9c6c09c72fa51ee3fe32096274e18ba32739e6a
qii <qiibeta@gmail.com>
2013-03-02 16:16:04 +0800

    edit my profile

--- src/org/qii/weiciyuan/ui/userinfo/EditMyProfileActivity.java
+++ src/org/qii/weiciyuan/ui/userinfo/EditMyProfileActivity.java
@@ -1,10 +1,18 @@
 package org.qii.weiciyuan.ui.userinfo;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -13,6 +21,7 @@ import org.qii.weiciyuan.bean.UserBean;
 import org.qii.weiciyuan.dao.user.EditMyProfileDao;
 import org.qii.weiciyuan.support.asyncdrawable.ProfileAvatarReadWorker;
 import org.qii.weiciyuan.support.error.WeiboException;
+import org.qii.weiciyuan.support.imagetool.ImageTool;
 import org.qii.weiciyuan.support.lib.MyAsyncTask;
 import org.qii.weiciyuan.support.utils.GlobalContext;
 import org.qii.weiciyuan.support.utils.Utility;
@@ -23,13 +32,20 @@ import org.qii.weiciyuan.ui.main.MainTimeLineActivity;
  * User: qii
  * Date: 13-2-28
  */
-public class EditMyProfileActivity extends AbstractAppActivity {
+public class EditMyProfileActivity extends AbstractAppActivity implements DialogInterface.OnClickListener {
+
+    private static final int CAMERA_RESULT = 0;
+    private static final int PIC_RESULT = 1;
 
     private UserBean userBean;
     private Layout layout;
 
     private ProfileAvatarReadWorker avatarTask;
     private SaveAsyncTask saveAsyncTask;
+    private NewProfileAvatarReaderWorker newProfileAvatarReaderWorker;
+
+    private Uri imageFileUri;
+    private String picPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +64,13 @@ public class EditMyProfileActivity extends AbstractAppActivity {
     private void initLayout() {
         layout = new Layout();
         layout.avatar = (ImageView) findViewById(R.id.avatar);
+        layout.avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SelectProfilePictureDialog dialog = new SelectProfilePictureDialog();
+                dialog.show(getFragmentManager(), "");
+            }
+        });
         layout.nickname = (EditText) findViewById(R.id.nickname);
         layout.website = (EditText) findViewById(R.id.website);
         layout.info = (EditText) findViewById(R.id.info);
@@ -70,6 +93,58 @@ public class EditMyProfileActivity extends AbstractAppActivity {
         if (Utility.isTaskStopped(saveAsyncTask)) {
             saveAsyncTask = new SaveAsyncTask();
             saveAsyncTask.executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
+        }
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        switch (which) {
+            case 0:
+                imageFileUri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                        new ContentValues());
+                if (imageFileUri != null) {
+                    Intent i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    i.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, imageFileUri);
+                    if (Utility.isIntentSafe(EditMyProfileActivity.this, i)) {
+                        startActivityForResult(i, CAMERA_RESULT);
+                    } else {
+                        Toast.makeText(EditMyProfileActivity.this, getString(R.string.dont_have_camera_app), Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(EditMyProfileActivity.this, getString(R.string.cant_insert_album), Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case 1:
+                Intent choosePictureIntent = new Intent(Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(choosePictureIntent, PIC_RESULT);
+                break;
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case CAMERA_RESULT:
+                    picPath = Utility.getPicPathFromUri(imageFileUri, this);
+                    break;
+                case PIC_RESULT:
+                    Uri imageFileUri = intent.getData();
+                    picPath = Utility.getPicPathFromUri(imageFileUri, this);
+                    break;
+            }
+        }
+        if (!TextUtils.isEmpty(picPath)) {
+            displayPic();
+        }
+    }
+
+
+    private void displayPic() {
+        if (Utility.isTaskStopped(newProfileAvatarReaderWorker)) {
+            newProfileAvatarReaderWorker = new NewProfileAvatarReaderWorker();
+            newProfileAvatarReaderWorker.executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
         }
     }
 
@@ -152,4 +227,34 @@ public class EditMyProfileActivity extends AbstractAppActivity {
         EditText info;
 
     }
+
+
+    private class NewProfileAvatarReaderWorker extends MyAsyncTask<String, Integer, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... url) {
+            if (isCancelled())
+                return null;
+
+
+            int avatarWidth = getResources().getDimensionPixelSize(R.dimen.profile_avatar_width);
+            int avatarHeight = getResources().getDimensionPixelSize(R.dimen.profile_avatar_height);
+
+            return ImageTool.getRoundedCornerPic(picPath, avatarWidth, avatarHeight);
+
+        }
+
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+
+            if (bitmap != null) {
+                layout.avatar.setImageBitmap(bitmap);
+            } else {
+                layout.avatar.setImageDrawable(new ColorDrawable(Color.TRANSPARENT));
+            }
+
+        }
+    }
+
 }
