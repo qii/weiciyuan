d5a5c4dc490467b996ab7663efa4b4019fee7eba
qii <qiibeta@gmail.com>
2013-03-10 18:52:49 +0800

    user dialog

--- src/org/qii/weiciyuan/ui/adapter/UserDialog.java
+++ src/org/qii/weiciyuan/ui/adapter/UserDialog.java
@@ -6,10 +6,16 @@ import android.app.DialogFragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.widget.Toast;
 import org.qii.weiciyuan.R;
 import org.qii.weiciyuan.bean.UserBean;
+import org.qii.weiciyuan.dao.relationship.FriendshipsDao;
 import org.qii.weiciyuan.support.database.FilterDBTask;
+import org.qii.weiciyuan.support.error.ErrorCode;
+import org.qii.weiciyuan.support.error.WeiboException;
+import org.qii.weiciyuan.support.lib.MyAsyncTask;
+import org.qii.weiciyuan.support.utils.AppLogger;
 import org.qii.weiciyuan.support.utils.GlobalContext;
 import org.qii.weiciyuan.ui.send.WriteWeiboActivity;
 import org.qii.weiciyuan.ui.userinfo.ManageGroupDialog;
@@ -48,17 +54,43 @@ public class UserDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-        CharSequence[] items = {"@他", "设置分组", "过滤", "取消关注"};
-        builder.setTitle(user.getScreen_name())
-                .setItems(items, new FriendOnClicker());
-
+        CharSequence[] friendItems = {getString(R.string.at_him), getString(R.string.manage_group), getString(R.string.add_to_app_filter), getString(R.string.unfollow_him)};
+        CharSequence[] strangerItems = {getString(R.string.at_him), getString(R.string.follow_him), getString(R.string.add_to_app_filter)};
+        if (user.isFollowing()) {
+            builder.setTitle(user.getScreen_name())
+                    .setItems(friendItems, new FriendOnClicker());
+        } else {
+            builder.setTitle(user.getScreen_name())
+                    .setItems(strangerItems, new StrangerOnClick());
+        }
 
         return builder.create();
     }
 
-    private class FriendOnClicker implements DialogInterface.OnClickListener {
+    private class StrangerOnClick implements DialogInterface.OnClickListener {
 
-        CharSequence[] items = {"@他", "设置分组", "过滤", "取消关注"};
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case 0:
+                    Intent intent = new Intent(getActivity(), WriteWeiboActivity.class);
+                    intent.putExtra("token", GlobalContext.getInstance().getSpecialToken());
+                    intent.putExtra("content", "@" + user.getScreen_name());
+                    intent.putExtra("account", GlobalContext.getInstance().getAccountBean());
+                    startActivity(intent);
+                    break;
+                case 1:
+                    new FollowTask().executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
+                    break;
+                case 2:
+                    FilterDBTask.addFilterKeyword(user.getScreen_name());
+                    Toast.makeText(getActivity(), getString(R.string.filter_successfully), Toast.LENGTH_SHORT).show();
+                    break;
+            }
+        }
+    }
+
+    private class FriendOnClicker implements DialogInterface.OnClickListener {
 
         @Override
         public void onClick(DialogInterface dialog, int which) {
@@ -79,8 +111,98 @@ public class UserDialog extends DialogFragment {
                     Toast.makeText(getActivity(), getString(R.string.filter_successfully), Toast.LENGTH_SHORT).show();
                     break;
                 case 3:
+                    new UnFollowTask().executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
                     break;
             }
         }
     }
+
+    private class FollowTask extends MyAsyncTask<Void, UserBean, UserBean> {
+        WeiboException e;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected UserBean doInBackground(Void... params) {
+
+            FriendshipsDao dao = new FriendshipsDao(GlobalContext.getInstance().getSpecialToken());
+            if (!TextUtils.isEmpty(user.getId())) {
+                dao.setUid(user.getId());
+            } else {
+                dao.setScreen_name(user.getScreen_name());
+            }
+
+            try {
+                return dao.followIt();
+            } catch (WeiboException e) {
+                AppLogger.e(e.getError());
+                this.e = e;
+                cancel(true);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onCancelled(UserBean userBean) {
+            super.onCancelled(userBean);
+        }
+
+        @Override
+        protected void onPostExecute(UserBean o) {
+            super.onPostExecute(o);
+            Toast.makeText(GlobalContext.getInstance(), GlobalContext.getInstance().getString(R.string.follow_successfully), Toast.LENGTH_SHORT).show();
+            user.setFollowing(true);
+        }
+    }
+
+    private class UnFollowTask extends MyAsyncTask<Void, UserBean, UserBean> {
+        WeiboException e;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected UserBean doInBackground(Void... params) {
+
+            FriendshipsDao dao = new FriendshipsDao(GlobalContext.getInstance().getSpecialToken());
+            if (!TextUtils.isEmpty(user.getId())) {
+                dao.setUid(user.getId());
+            } else {
+                dao.setScreen_name(user.getScreen_name());
+            }
+
+            try {
+                return dao.unFollowIt();
+            } catch (WeiboException e) {
+                AppLogger.e(e.getError());
+                this.e = e;
+                cancel(true);
+                return null;
+            }
+        }
+
+        @Override
+        protected void onCancelled(UserBean userBean) {
+            super.onCancelled(userBean);
+            if (this.e != null) {
+                if (this.e.getError_code() == ErrorCode.NOT_FOLLOWED) {
+                    user.setFollowing(false);
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(UserBean o) {
+            super.onPostExecute(o);
+            Toast.makeText(GlobalContext.getInstance(), GlobalContext.getInstance().getString(R.string.unfollow_successfully), Toast.LENGTH_SHORT).show();
+            user.setFollowing(false);
+        }
+    }
+
+
 }
