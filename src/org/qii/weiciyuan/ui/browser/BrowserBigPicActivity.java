8827013c47295065352abfe542d8fa2cfe3f0521
qii <qiibeta@gmail.com>
2013-03-21 21:48:08 +0800

    view pic

--- src/org/qii/weiciyuan/ui/browser/BrowserBigPicActivity.java
+++ src/org/qii/weiciyuan/ui/browser/BrowserBigPicActivity.java
@@ -1,10 +1,14 @@
 package org.qii.weiciyuan.ui.browser;
 
-import android.app.ActionBar;
+import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.content.res.TypedArray;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -12,7 +16,7 @@ import android.os.Handler;
 import android.text.TextUtils;
 import android.view.*;
 import android.webkit.WebView;
-import android.widget.ShareActionProvider;
+import android.widget.ImageButton;
 import android.widget.Toast;
 import org.qii.weiciyuan.R;
 import org.qii.weiciyuan.bean.MessageBean;
@@ -21,11 +25,10 @@ import org.qii.weiciyuan.support.file.FileDownloaderHttpHelper;
 import org.qii.weiciyuan.support.file.FileLocationMethod;
 import org.qii.weiciyuan.support.file.FileManager;
 import org.qii.weiciyuan.support.imagetool.ImageTool;
+import org.qii.weiciyuan.support.lib.CheatSheet;
 import org.qii.weiciyuan.support.lib.CircleProgressView;
 import org.qii.weiciyuan.support.lib.MyAsyncTask;
 import org.qii.weiciyuan.support.utils.Utility;
-import org.qii.weiciyuan.ui.interfaces.AbstractAppActivity;
-import org.qii.weiciyuan.ui.main.MainTimeLineActivity;
 
 import java.io.File;
 import java.util.List;
@@ -34,41 +37,45 @@ import java.util.List;
  * User: qii
  * Date: 12-8-18
  */
-public class BrowserBigPicActivity extends AbstractAppActivity {
+public class BrowserBigPicActivity extends Activity {
 
-
-    private MenuItem oriMenu;
-    private WebView webView;
-    private CircleProgressView pb;
     private PicSimpleBitmapWorkerTask task;
     private PicSaveTask saveTask;
     private String path;
-    private ShareActionProvider mShareActionProvider;
     private MessageBean msg;
+    private Layout layout;
+    private boolean hd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        getWindow().setBackgroundDrawable(new ColorDrawable(0));
         setContentView(R.layout.browserbigpicactivity_layout);
-        ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
-        actionBar.setTitle(R.string.browser_picture);
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT;
+        getWindow().setAttributes((android.view.WindowManager.LayoutParams) params);
 
-        pb = (CircleProgressView) findViewById(R.id.pb);
+        layout = new Layout();
+        layout.refresh = (ImageButton) findViewById(R.id.ib_refresh);
+        layout.copy = (ImageButton) findViewById(R.id.ib_copy);
+        layout.share = (ImageButton) findViewById(R.id.ib_share);
+        layout.save = (ImageButton) findViewById(R.id.ib_save);
+        layout.hd = (ImageButton) findViewById(R.id.ib_hd);
 
-        webView = (WebView) findViewById(R.id.iv);
+        layout.pb = (CircleProgressView) findViewById(R.id.pb);
 
-        webView.setBackgroundColor(getResources().getColor(R.color.transparent));
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setUseWideViewPort(true);
-        webView.getSettings().setLoadWithOverviewMode(true);
-        webView.getSettings().setBuiltInZoomControls(true);
-        webView.getSettings().setDisplayZoomControls(false);
+        layout.webView = (WebView) findViewById(R.id.iv);
 
-        webView.setVerticalScrollBarEnabled(false);
-        webView.setHorizontalScrollBarEnabled(false);
-        webView.setOnTouchListener(new View.OnTouchListener() {
+        layout.webView.setBackgroundColor(getResources().getColor(R.color.transparent));
+        layout.webView.getSettings().setJavaScriptEnabled(true);
+        layout.webView.getSettings().setUseWideViewPort(true);
+        layout.webView.getSettings().setLoadWithOverviewMode(true);
+        layout.webView.getSettings().setBuiltInZoomControls(true);
+        layout.webView.getSettings().setDisplayZoomControls(false);
+
+        layout.webView.setVerticalScrollBarEnabled(false);
+        layout.webView.setHorizontalScrollBarEnabled(false);
+        layout.webView.setOnTouchListener(new View.OnTouchListener() {
             boolean mPressed;
             boolean mClose;
             CheckForSinglePress mPendingCheckForSinglePress = new CheckForSinglePress();
@@ -129,7 +136,90 @@ public class BrowserBigPicActivity extends AbstractAppActivity {
             task = new PicSimpleBitmapWorkerTask();
             task.executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
         }
+
+        initOnClickListener();
+
+        String largePath = FileManager.getFilePathFromUrl(msg.getOriginal_pic(), FileLocationMethod.picture_large);
+        if (new File(largePath).exists()) {
+            hd = true;
+        }
     }
+
+    private void initOnClickListener() {
+        layout.refresh.setOnClickListener(onClickListener);
+        layout.copy.setOnClickListener(onClickListener);
+        layout.share.setOnClickListener(onClickListener);
+        layout.save.setOnClickListener(onClickListener);
+        layout.hd.setOnClickListener(onClickListener);
+
+        CheatSheet.setup(this, layout.refresh, R.string.refresh);
+        CheatSheet.setup(this, layout.copy, R.string.copy_link_to_clipboard);
+        CheatSheet.setup(this, layout.share, R.string.share);
+        CheatSheet.setup(this, layout.save, R.string.save_pic_album);
+        CheatSheet.setup(this, layout.hd, R.string.switch_to_ori_pic);
+    }
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.ib_refresh:
+                    if (task != null) {
+                        task.cancel(true);
+                    }
+
+                    if (!TextUtils.isEmpty(path)) {
+                        new File(path).delete();
+                    }
+                    if (!hd) {
+                        task = new PicSimpleBitmapWorkerTask();
+                    } else {
+                        task = new PicSimpleBitmapWorkerTask();
+                    }
+                    task.executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
+
+                    break;
+                case R.id.ib_copy:
+                    ClipboardManager cm = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+                    cm.setPrimaryClip(ClipData.newPlainText("sinaweibo", msg.getOriginal_pic()));
+                    Toast.makeText(BrowserBigPicActivity.this, getString(R.string.copy_successfully), Toast.LENGTH_SHORT).show();
+                    break;
+                case R.id.ib_share:
+                    Intent sharingIntent = new Intent(Intent.ACTION_SEND);
+                    sharingIntent.setType("image/jpeg");
+                    if (!TextUtils.isEmpty(path)) {
+                        Uri uri = Uri.fromFile(new File(path));
+                        sharingIntent.putExtra(Intent.EXTRA_STREAM, uri);
+                        PackageManager packageManager = getPackageManager();
+                        List<ResolveInfo> activities = packageManager.queryIntentActivities(sharingIntent, 0);
+                        boolean isIntentSafe = activities.size() > 0;
+                        if (isIntentSafe) {
+                            startActivity(Intent.createChooser(sharingIntent, getString(R.string.share)));
+                        }
+                    }
+                    break;
+                case R.id.ib_save:
+                    if (task != null && task.getStatus() == MyAsyncTask.Status.FINISHED) {
+                        if (saveTask == null) {
+                            saveTask = new PicSaveTask();
+                            saveTask.executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
+                        } else if (saveTask.getStatus() == MyAsyncTask.Status.FINISHED) {
+                            Toast.makeText(BrowserBigPicActivity.this, getString(R.string.already_saved), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    break;
+                case R.id.ib_hd:
+                    if (task != null) {
+                        task.cancel(true);
+                    }
+
+                    task = new PicSimpleBitmapWorkerTask();
+                    task.executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
+                    path = "";
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
@@ -137,96 +227,11 @@ public class BrowserBigPicActivity extends AbstractAppActivity {
         outState.putSerializable("msg", msg);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.actionbar_menu_browserbigpicactivity, menu);
-        MenuItem item = menu.findItem(R.id.menu_share);
-        mShareActionProvider = (ShareActionProvider) item.getActionProvider();
-        oriMenu = menu.findItem(R.id.menu_switch);
-        String largePath = FileManager.getFilePathFromUrl(msg.getOriginal_pic(), FileLocationMethod.picture_large);
-        if (new File(largePath).exists()) {
-            oriMenu.setVisible(false);
-        }
-        return super.onCreateOptionsMenu(menu);
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                Intent intent = new Intent(this, MainTimeLineActivity.class);
-                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-                return true;
-            case R.id.menu_switch:
-                if (task != null) {
-                    task.cancel(true);
-                }
-
-                task = new PicSimpleBitmapWorkerTask();
-                task.executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
-                path = "";
-                oriMenu.setVisible(false);
-                getActionBar().setSubtitle(null);
-                break;
-            case R.id.menu_save:
-                if (task != null && task.getStatus() == MyAsyncTask.Status.FINISHED) {
-                    if (saveTask == null) {
-                        saveTask = new PicSaveTask();
-                        saveTask.executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
-                    } else if (saveTask.getStatus() == MyAsyncTask.Status.FINISHED) {
-                        Toast.makeText(BrowserBigPicActivity.this, getString(R.string.already_saved), Toast.LENGTH_SHORT).show();
-                    }
-                }
-                break;
-            case R.id.menu_share:
-
-                Intent sharingIntent = new Intent(Intent.ACTION_SEND);
-                sharingIntent.setType("image/jpeg");
-
-                if (!TextUtils.isEmpty(path)) {
-                    Uri uri = Uri.fromFile(new File(path));
-                    sharingIntent.putExtra(Intent.EXTRA_STREAM, uri);
-                    PackageManager packageManager = getPackageManager();
-                    List<ResolveInfo> activities = packageManager.queryIntentActivities(sharingIntent, 0);
-                    boolean isIntentSafe = activities.size() > 0;
-                    if (isIntentSafe && mShareActionProvider != null) {
-                        mShareActionProvider.setShareIntent(sharingIntent);
-                    }
-                }
-
-                break;
-            case R.id.menu_download:
-                if (task != null) {
-                    task.cancel(true);
-                }
-
-                if (!TextUtils.isEmpty(path)) {
-                    new File(path).delete();
-                }
-                if (oriMenu.isVisible()) {
-                    task = new PicSimpleBitmapWorkerTask();
-                } else {
-                    task = new PicSimpleBitmapWorkerTask();
-                }
-                task.executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
-
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    class PicSaveTask extends MyAsyncTask<Void, Boolean, Boolean> {
-
+    private class PicSaveTask extends MyAsyncTask<Void, Boolean, Boolean> {
 
         @Override
         protected Boolean doInBackground(Void... params) {
-//            try {
-//                MediaStore.Images.Media.insertImage(getContentResolver(), path, "", "");
-//            } catch (FileNotFoundException e) {
-//                AppLogger.e(e.getMessage());
-//                cancel(true);
-//            }
             return FileManager.saveToPicDir(path);
         }
 
@@ -260,8 +265,8 @@ public class BrowserBigPicActivity extends AbstractAppActivity {
         protected void onPreExecute() {
             super.onPreExecute();
 //            pb.setIndeterminate(true);
-            pb.setVisibility(View.VISIBLE);
-            webView.setVisibility(View.INVISIBLE);
+            layout.pb.setVisibility(View.VISIBLE);
+            layout.webView.setVisibility(View.INVISIBLE);
         }
 
         @Override
@@ -294,8 +299,8 @@ public class BrowserBigPicActivity extends AbstractAppActivity {
             int progress = values[0];
             int max = values[1];
 //            pb.setIndeterminate(false);
-            pb.setMax(max);
-            pb.setProgress(progress);
+            layout.pb.setMax(max);
+            layout.pb.setProgress(progress);
         }
 
         @Override
@@ -314,21 +319,16 @@ public class BrowserBigPicActivity extends AbstractAppActivity {
             if (!TextUtils.isEmpty(bitmapPath)) {
                 path = bitmapPath;
 
-                if (ImageTool.isThisBitmapCanRead(bitmapPath)) {
-                    int[] size = ImageTool.getBitmapSize(bitmapPath);
-                    getActionBar().setSubtitle(String.valueOf(size[0]) + "x" + String.valueOf(size[1]));
-                }
-
                 File file = new File(bitmapPath);
 
                 String str1 = "file://" + file.getAbsolutePath().replace("/mnt/sdcard/", "/sdcard/");
                 String str2 = "<html>\n<head>\n     <style>\n          html,body{background:transparent;margin:0;padding:0;}          *{-webkit-tap-highlight-color:rgba(0, 0, 0, 0);}\n     </style>\n     <script type=\"text/javascript\">\n     var imgUrl = \"" + str1 + "\";" + "     var objImage = new Image();\n" + "     var realWidth = 0;\n" + "     var realHeight = 0;\n" + "\n" + "     function onLoad() {\n" + "          objImage.onload = function() {\n" + "               realWidth = objImage.width;\n" + "               realHeight = objImage.height;\n" + "\n" + "               document.gagImg.src = imgUrl;\n" + "               onResize();\n" + "          }\n" + "          objImage.src = imgUrl;\n" + "     }\n" + "\n" + "     function onResize() {\n" + "          var scale = 1;\n" + "          var newWidth = document.gagImg.width;\n" + "          if (realWidth > newWidth) {\n" + "               scale = realWidth / newWidth;\n" + "          } else {\n" + "               scale = newWidth / realWidth;\n" + "          }\n" + "\n" + "          hiddenHeight = Math.ceil(30 * scale);\n" + "          document.getElementById('hiddenBar').style.height = hiddenHeight + \"px\";\n" + "          document.getElementById('hiddenBar').style.marginTop = -hiddenHeight + \"px\";\n" + "     }\n" + "     </script>\n" + "</head>\n" + "<body onload=\"onLoad()\" onresize=\"onResize()\" onclick=\"Android.toggleOverlayDisplay();\">\n" + "     <table style=\"width: 100%;height:100%;\">\n" + "          <tr style=\"width: 100%;\">\n" + "               <td valign=\"middle\" align=\"center\" style=\"width: 100%;\">\n" + "                    <div style=\"display:block\">\n" + "                         <img name=\"gagImg\" src=\"\" width=\"100%\" style=\"\" />\n" + "                    </div>\n" + "                    <div id=\"hiddenBar\" style=\"position:absolute; width: 100%; background: transparent;\"></div>\n" + "               </td>\n" + "          </tr>\n" + "     </table>\n" + "</body>\n" + "</html>";
-                webView.loadDataWithBaseURL("file:///android_asset/", str2, "text/html", "utf-8", null);
-                webView.setVisibility(View.VISIBLE);
-                pb.setVisibility(View.INVISIBLE);
-
+                layout.webView.loadDataWithBaseURL("file:///android_asset/", str2, "text/html", "utf-8", null);
+                layout.webView.setVisibility(View.VISIBLE);
+                layout.pb.setVisibility(View.INVISIBLE);
+                hd = true;
             } else {
-                pb.setVisibility(View.GONE);
+                layout.pb.setVisibility(View.GONE);
                 int[] attrs = new int[]{R.attr.error};
                 TypedArray ta = BrowserBigPicActivity.this.obtainStyledAttributes(attrs);
                 Drawable drawableFromTheme = ta.getDrawable(0);
@@ -344,7 +344,18 @@ public class BrowserBigPicActivity extends AbstractAppActivity {
     protected void onDestroy() {
         super.onDestroy();
         Utility.cancelTasks(task, saveTask);
-        webView.loadUrl("about:blank");
-        webView.stopLoading();
+        layout.webView.loadUrl("about:blank");
+        layout.webView.stopLoading();
+    }
+
+    private class Layout {
+        WebView webView;
+        CircleProgressView pb;
+
+        ImageButton refresh;
+        ImageButton copy;
+        ImageButton share;
+        ImageButton save;
+        ImageButton hd;
     }
 }
