53d920767617c382bbc055e86c165b74b1c50e10
qii <qiibeta@gmail.com>
2013-05-01 19:57:07 +0800

    fix

--- src/org/qii/weiciyuan/support/asyncdrawable/ReadWorker.java
+++ src/org/qii/weiciyuan/support/asyncdrawable/ReadWorker.java
@@ -99,7 +99,14 @@ public class ReadWorker extends MyAsyncTask<String, Void, Bitmap> implements IPi
                 width = (int) (metrics.widthPixels - (14 + 14) * reSize);
         }
 
-
+        synchronized (TimeLineBitmapDownloader.pauseWorkLock) {
+            while (TimeLineBitmapDownloader.pauseWork && !isCancelled()) {
+                try {
+                    TimeLineBitmapDownloader.pauseWorkLock.wait();
+                } catch (InterruptedException e) {
+                }
+            }
+        }
         Bitmap bitmap = ImageTool.getRoundedCornerPic(path, width, height);
         if (bitmap == null) {
             this.failedResult = FailedResult.readFailed;
