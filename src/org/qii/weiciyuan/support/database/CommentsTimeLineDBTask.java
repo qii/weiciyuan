bd3cc22c8b1c50d7453bd41521b9a1a33bb51b05
qii <qiibeta@gmail.com>
2013-05-30 20:53:31 +0800

    fix

--- src/org/qii/weiciyuan/support/database/CommentsTimeLineDBTask.java
+++ src/org/qii/weiciyuan/support/database/CommentsTimeLineDBTask.java
@@ -75,7 +75,8 @@ public class CommentsTimeLineDBTask {
             String json = c.getString(c.getColumnIndex(CommentsTable.CommentsDataTable.JSONDATA));
             try {
                 CommentBean value = gson.fromJson(json, CommentBean.class);
-                value.getListViewSpannableString();
+                if (value != null)
+                    value.getListViewSpannableString();
                 msgList.add(value);
             } catch (JsonSyntaxException e) {
                 AppLogger.e(e.getMessage());
