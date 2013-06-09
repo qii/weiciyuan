80da1c10973f23cee1da0a736e35758adedb0dbd
qii <qiibeta@gmail.com>
2013-06-09 19:44:58 +0800

    Parcelable

--- src/org/qii/weiciyuan/bean/FavBean.java
+++ src/org/qii/weiciyuan/bean/FavBean.java
@@ -1,16 +1,46 @@
 package org.qii.weiciyuan.bean;
 
+import android.os.Parcel;
+import android.os.Parcelable;
 import org.qii.weiciyuan.support.utils.ObjectToStringUtility;
-
-import java.io.Serializable;
 
 /**
  * User: qii
  * Date: 12-8-18
  */
-public class FavBean implements Serializable {
+public class FavBean implements Parcelable {
     private MessageBean status;
     private String favorited_time;
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+
+        dest.writeParcelable(status, flags);
+        dest.writeString(favorited_time);
+
+    }
+
+    public static final Parcelable.Creator<FavBean> CREATOR =
+            new Parcelable.Creator<FavBean>() {
+                public FavBean createFromParcel(Parcel in) {
+                    FavBean favBean = new FavBean();
+
+                    favBean.status = in.readParcelable(MessageBean.class.getClassLoader());
+                    favBean.favorited_time = in.readString();
+
+                    return favBean;
+                }
+
+                public FavBean[] newArray(int size) {
+                    return new FavBean[size];
+                }
+            };
+
 
     public MessageBean getStatus() {
         return status;
