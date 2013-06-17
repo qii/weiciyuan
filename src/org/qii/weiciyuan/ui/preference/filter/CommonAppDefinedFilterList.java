b2f2ac8ccdd0c93b3e74245cf5a492eef2e726c1
qii <qiibeta@gmail.com>
2013-06-17 22:53:24 +0800

    fix

--- src/org/qii/weiciyuan/ui/preference/filter/CommonAppDefinedFilterList.java
+++ src/org/qii/weiciyuan/ui/preference/filter/CommonAppDefinedFilterList.java
@@ -11,7 +11,7 @@ import java.util.Set;
 public class CommonAppDefinedFilterList {
     public static Set<String> getDefinedFilterKeywordAndUserList() {
         Set<String> result = new LinkedHashSet<String>();
-        Collections.addAll(result, "十二星座", "成功学", "不转不是中国人");
+        Collections.addAll(result, "十二星座", "成功学", "不转不是中国人", "经典语录");
         Collections.addAll(result, "白羊座女人", "白羊座男人", "金牛座女人", "金牛座男人",
                 "双子座女人", "双子座男人", "巨蟹座女人", "巨蟹座男人", "狮子座女人", "狮子座男人",
                 "处女座女人", "处女座男人", "天秤座女人", "天秤座男人", "摩羯座女人", "摩羯座男人",
