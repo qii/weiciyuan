e50be75c26cef1fd105e513946153685603b4a6c
qii <qiibeta@gmail.com>
2013-09-14 19:18:18 +0800

    gallery switch animation

--- src/org/qii/weiciyuan/support/gallery/GalleryActivity.java
+++ src/org/qii/weiciyuan/support/gallery/GalleryActivity.java
@@ -82,6 +82,7 @@ public class GalleryActivity extends Activity {
         });
         pager.setCurrentItem(getIntent().getIntExtra("position", 0));
         pager.setOffscreenPageLimit(3);
+        pager.setPageTransformer(true, new ZoomOutPageTransformer());
     }
 
     @Override
