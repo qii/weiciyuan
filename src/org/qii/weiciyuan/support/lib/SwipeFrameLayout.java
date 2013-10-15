76780a736197723b385bb7e1e6d94f0f8a8fefea
qii <qiibeta@gmail.com>
2013-10-15 21:48:38 +0800

    x

---
+++ src/org/qii/weiciyuan/support/lib/SwipeFrameLayout.java
@@ -0,0 +1,190 @@
+package org.qii.weiciyuan.support.lib;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
+import org.qii.weiciyuan.R;
+import org.qii.weiciyuan.support.utils.ThemeUtility;
+import org.qii.weiciyuan.support.utils.Utility;
+
+/**
+ * User: qii
+ * Date: 13-10-15
+ */
+public class SwipeFrameLayout extends FrameLayout {
+
+    private Activity activity;
+
+    private boolean isDragging = false;
+    private float[] initPointLocation = new float[2];
+    private View topView;
+
+    private static final int OFFSET = 5;
+
+    private int max_motion_event_down_x_position;
+
+    private GestureDetector gestureDetector;
+
+    public SwipeFrameLayout(Context context) {
+        this(context, null);
+    }
+
+    public SwipeFrameLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, -1);
+    }
+
+    public SwipeFrameLayout(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    private void init() {
+        setBackground(ThemeUtility.getDrawable(android.R.attr.windowBackground));
+        this.activity = (Activity) getContext();
+        this.topView = ((View) (activity.findViewById(android.R.id.content).getParent()));
+        this.max_motion_event_down_x_position = Utility.dip2px(25);
+        this.gestureDetector = new GestureDetector(getContext(), new SwipeRightToCloseOnGestureListener());
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+
+        switch (ev.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                initPointLocation[0] = ev.getRawX();
+                initPointLocation[1] = ev.getRawY();
+
+                return false;
+            case MotionEvent.ACTION_MOVE:
+                if (isDragging) {
+                    gestureDetector.onTouchEvent(ev);
+                    return true;
+                }
+
+                float xx = ev.getRawX();
+                if ((xx > initPointLocation[0] + Utility.dip2px(OFFSET)) && initPointLocation[0] <= max_motion_event_down_x_position) {
+                    isDragging = true;
+                    gestureDetector.onTouchEvent(ev);
+                    return true;
+                }
+
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                isDragging = false;
+
+                if (initPointLocation[0] <= max_motion_event_down_x_position) {
+                    int x = (int) (ev.getRawX() - initPointLocation[0]);
+                    initPointLocation[0] = 0f;
+                    initPointLocation[1] = 0f;
+
+
+                    if (x > (Utility.getScreenWidth() / 2)) {
+                        closeActivity();
+                        return true;
+                    } else {
+                        restoreActivity();
+                    }
+                }
+                break;
+        }
+
+        return super.onTouchEvent(ev);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+
+        switch (ev.getActionMasked()) {
+            case MotionEvent.ACTION_DOWN:
+                initPointLocation[0] = ev.getRawX();
+                initPointLocation[1] = ev.getRawY();
+                this.gestureDetector.onTouchEvent(ev);
+                break;
+        }
+
+
+        return super.dispatchTouchEvent(ev);
+
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+
+        if (isDragging) {
+            return true;
+        }
+
+
+        switch (ev.getActionMasked()) {
+            case MotionEvent.ACTION_MOVE:
+                float x = ev.getRawX();
+                if (x > initPointLocation[0] && initPointLocation[0] <= max_motion_event_down_x_position) {
+                    return true;
+                }
+                break;
+
+        }
+        return super.onInterceptTouchEvent(ev);
+    }
+
+
+    private void closeActivity() {
+        activity.finish();
+        activity.overridePendingTransition(R.anim.stay, R.anim.swipe_right_to_close);
+    }
+
+    private void restoreActivity() {
+
+        topView.animate().translationX(0)
+                .setDuration(300L).withLayer().setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                topView.animate().setListener(null);
+            }
+        });
+    }
+
+    private class SwipeRightToCloseOnGestureListener extends GestureDetector.SimpleOnGestureListener {
+        protected MotionEvent mLastOnDownEvent = null;
+        private float[] initPointLocation = new float[2];
+
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            mLastOnDownEvent = e;
+            initPointLocation[0] = e.getRawX();
+            initPointLocation[1] = e.getRawY();
+            return true;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            if (e2.getRawX() < initPointLocation[0]) {
+                float y = topView.getTranslationX();
+                if (y != 0f) {
+                    topView.setTranslationX(0);
+                    return super.onScroll(e1, e2, distanceX, distanceY);
+                } else {
+                    return false;
+                }
+            }
+            final int MAX_MOTION_EVENT_DOWN__X_POSITION = Utility.dip2px(25);
+            float s = e2.getRawX() - initPointLocation[0];
+            if (initPointLocation[0] <= MAX_MOTION_EVENT_DOWN__X_POSITION) {
+                topView.setTranslationX(s);
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+    }
+
+}
