ee1ff294aa8c51789838a313495db2e2570d728e
qii <qiibeta@gmail.com>
2014-01-05 21:25:52 +0800

    refactor sso login option

--- src/org/qii/weiciyuan/ui/login/SSOActivity.java
+++ src/org/qii/weiciyuan/ui/login/SSOActivity.java
@@ -1,21 +1,24 @@
 package org.qii.weiciyuan.ui.login;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Toast;
 import org.qii.weiciyuan.R;
 import org.qii.weiciyuan.bean.AccountBean;
 import org.qii.weiciyuan.bean.UserBean;
 import org.qii.weiciyuan.dao.login.OAuthDao;
 import org.qii.weiciyuan.support.database.AccountDBTask;
+import org.qii.weiciyuan.support.debug.AppLogger;
 import org.qii.weiciyuan.support.error.WeiboException;
 import org.qii.weiciyuan.support.lib.MyAsyncTask;
 import org.qii.weiciyuan.support.lib.sinasso.SsoHandler;
-import org.qii.weiciyuan.support.debug.AppLogger;
 import org.qii.weiciyuan.support.utils.Utility;
 import org.qii.weiciyuan.ui.interfaces.AbstractAppActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.lang.ref.WeakReference;
 
 /**
  * User: qii
@@ -29,8 +32,10 @@ public class SSOActivity extends AbstractAppActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActionBar().setTitle(R.string.official_app_login);
-        SsoHandler mSsoHandler = new SsoHandler(SSOActivity.this);
-        mSsoHandler.authorize();
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setDisplayShowHomeEnabled(false);
+        SsoHandler ssoHandler = new SsoHandler(SSOActivity.this);
+        ssoHandler.authorize();
     }
 
     @Override
@@ -39,17 +44,22 @@ public class SSOActivity extends AbstractAppActivity {
         Utility.cancelTasks(task);
     }
 
-    private class SSOTask extends MyAsyncTask<String, UserBean, OAuthActivity.DBResult> {
+    private static class SSOTask extends MyAsyncTask<String, UserBean, OAuthActivity.DBResult> {
 
-        WeiboException e;
+        private WeakReference<SSOActivity> sSOActivityWeakReference;
 
-        OAuthActivity.ProgressFragment progressFragment = OAuthActivity.ProgressFragment.newInstance();
+        private WeiboException e;
+
+        private OAuthActivity.ProgressFragment progressFragment = OAuthActivity.ProgressFragment
+                .newInstance();
 
         private String token;
+
         private String expiresIn;
 
 
-        public SSOTask(String token, String expiresIn) {
+        public SSOTask(SSOActivity ssoActivity, String token, String expiresIn) {
+            this.sSOActivityWeakReference = new WeakReference<SSOActivity>(ssoActivity);
             this.token = token;
             this.expiresIn = expiresIn;
         }
@@ -57,7 +67,11 @@ public class SSOActivity extends AbstractAppActivity {
         @Override
         protected void onPreExecute() {
             progressFragment.setAsyncTask(this);
-            progressFragment.show(getSupportFragmentManager(), "");
+
+            SSOActivity activity = sSOActivityWeakReference.get();
+            if (activity != null) {
+                progressFragment.show(activity.getSupportFragmentManager(), "");
+            }
 
         }
 
@@ -68,9 +82,11 @@ public class SSOActivity extends AbstractAppActivity {
                 UserBean user = new OAuthDao(token).getOAuthUserInfo();
                 AccountBean account = new AccountBean();
                 account.setAccess_token(token);
-                account.setExpires_time(System.currentTimeMillis() + Long.valueOf(expiresIn) * 1000);
+                account.setExpires_time(
+                        System.currentTimeMillis() + Long.valueOf(expiresIn) * 1000);
                 account.setInfo(user);
-                AppLogger.e("token expires in " + Utility.calcTokenExpiresInDays(account) + " days");
+                AppLogger
+                        .e("token expires in " + Utility.calcTokenExpiresInDays(account) + " days");
                 return AccountDBTask.addOrUpdateAccount(account, false);
             } catch (WeiboException e) {
                 AppLogger.e(e.getError());
@@ -87,8 +103,18 @@ public class SSOActivity extends AbstractAppActivity {
             if (progressFragment != null) {
                 progressFragment.dismissAllowingStateLoss();
             }
-            if (e != null)
-                Toast.makeText(SSOActivity.this, e.getError(), Toast.LENGTH_SHORT).show();
+
+            SSOActivity activity = sSOActivityWeakReference.get();
+            if (activity == null) {
+                return;
+            }
+
+            if (e != null) {
+                Toast.makeText(activity, e.getError(), Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(activity, R.string.you_cancel_login, Toast.LENGTH_SHORT).show();
+            }
+            activity.finish();
 
         }
 
@@ -97,21 +123,29 @@ public class SSOActivity extends AbstractAppActivity {
             if (progressFragment.isVisible()) {
                 progressFragment.dismissAllowingStateLoss();
             }
+
+            SSOActivity activity = sSOActivityWeakReference.get();
+            if (activity == null) {
+                return;
+            }
+
             switch (dbResult) {
                 case add_successfuly:
                     Bundle values = new Bundle();
                     values.putString("expires_in", expiresIn);
                     Intent intent = new Intent();
                     intent.putExtras(values);
-                    setResult(RESULT_OK, intent);
-                    finish();
-                    Toast.makeText(SSOActivity.this, getString(R.string.login_success), Toast.LENGTH_SHORT).show();
+                    activity.setResult(RESULT_OK, intent);
+                    activity.finish();
+                    Toast.makeText(activity, activity.getString(R.string.login_success),
+                            Toast.LENGTH_SHORT).show();
                     break;
                 case update_successfully:
-                    Toast.makeText(SSOActivity.this, getString(R.string.update_account_success), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(activity, activity.getString(R.string.update_account_success),
+                            Toast.LENGTH_SHORT).show();
                     break;
             }
-            finish();
+            activity.finish();
 
         }
     }
@@ -120,10 +154,11 @@ public class SSOActivity extends AbstractAppActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_CANCELED) {
+        if (resultCode == Activity.RESULT_CANCELED || data == null) {
+            Toast.makeText(this, R.string.you_cancel_login, Toast.LENGTH_SHORT).show();
+            finish();
             return;
         }
-
 
         // Check OAuth 2.0/2.10 error code.
         String error = data.getStringExtra("error");
@@ -136,7 +171,8 @@ public class SSOActivity extends AbstractAppActivity {
             if (error.equals("access_denied")
                     || error.equals("OAuthAccessDeniedException")) {
                 Log.d("Weibo-authorize", "Login canceled by user.");
-
+                Toast.makeText(this, R.string.you_cancel_login, Toast.LENGTH_SHORT).show();
+                finish();
             } else {
                 String description = data
                         .getStringExtra("error_description");
@@ -144,6 +180,9 @@ public class SSOActivity extends AbstractAppActivity {
                     error = error + ":" + description;
                 }
                 Log.d("Weibo-authorize", "Login failed: " + error);
+                Toast.makeText(this, getString(R.string.login_failed) + error, Toast.LENGTH_SHORT)
+                        .show();
+                finish();
 
             }
             return;
@@ -160,7 +199,7 @@ public class SSOActivity extends AbstractAppActivity {
         String refreshToken = data.getStringExtra(KEY_REFRESHTOKEN);
 
         if (Utility.isTaskStopped(task)) {
-            task = new SSOTask(token, expires);
+            task = new SSOTask(SSOActivity.this, token, expires);
             task.executeOnExecutor(MyAsyncTask.THREAD_POOL_EXECUTOR);
         }
     }
