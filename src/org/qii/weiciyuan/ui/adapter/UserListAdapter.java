f389148d215238a67a6c2c15bbe29f70e84abfcb
qii <qiibeta@gmail.com>
2014-03-09 13:20:23 +0800

    fix crash

--- src/org/qii/weiciyuan/ui/adapter/UserListAdapter.java
+++ src/org/qii/weiciyuan/ui/adapter/UserListAdapter.java
@@ -92,7 +92,7 @@ public class UserListAdapter extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
 
         ViewHolder holder;
-        if (convertView == null) {
+        if (convertView == null || convertView.getTag() == null) {
             holder = new ViewHolder();
             convertView = inflater.inflate(R.layout.user_listview_item_layout, parent, false);
             holder.username = ViewUtility.findViewById(convertView, R.id.username);
