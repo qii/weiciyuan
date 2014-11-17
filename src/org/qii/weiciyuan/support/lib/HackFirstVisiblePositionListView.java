b73393ddeade859b0aa6bc4a5d9e68d0be4c63c8
qii <qiibeta@gmail.com>
2014-11-17 20:57:55 +0800

    hack to fix getFirstVisiblePosition

--- src/org/qii/weiciyuan/support/lib/HackFirstVisiblePositionListView.java
+++ src/org/qii/weiciyuan/support/lib/HackFirstVisiblePositionListView.java
@@ -2,7 +2,11 @@ package org.qii.weiciyuan.support.lib;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.AbsListView;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * hack to fix getFirstVisiblePosition
@@ -12,21 +16,32 @@ import android.widget.ListView;
 public class HackFirstVisiblePositionListView extends ListView {
 
     private int mCorrectFirstVisiblePosition = -1;
+    private final ForwardingOnScrollListener mForwardingOnScrollListener
+            = new ForwardingOnScrollListener();
 
     public HackFirstVisiblePositionListView(Context context) {
         super(context);
+        init();
     }
 
     public HackFirstVisiblePositionListView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public HackFirstVisiblePositionListView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     public HackFirstVisiblePositionListView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        super.setOnScrollListener(mForwardingOnScrollListener);
+        mForwardingOnScrollListener.selfListener = mOnScrollListener;
     }
 
     @Override
@@ -51,4 +66,52 @@ public class HackFirstVisiblePositionListView extends ListView {
     public int getFirstVisiblePosition() {
         return mCorrectFirstVisiblePosition;
     }
+
+    @Override
+    public void setOnScrollListener(OnScrollListener l) {
+        mForwardingOnScrollListener.clientListener.add(l);
+    }
+
+    private static class ForwardingOnScrollListener implements OnScrollListener {
+
+        private OnScrollListener selfListener;
+
+        private List<OnScrollListener> clientListener = new ArrayList<OnScrollListener>();
+
+        @Override
+        public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount,
+                int totalItemCount) {
+
+            if (selfListener != null) {
+                selfListener.onScroll(view, firstVisibleItem, visibleItemCount, totalItemCount);
+            }
+
+            for (OnScrollListener l : clientListener) {
+                l.onScroll(view, firstVisibleItem, visibleItemCount, totalItemCount);
+            }
+        }
+
+        @Override
+        public void onScrollStateChanged(AbsListView view, int scrollState) {
+            if (selfListener != null) {
+                selfListener.onScrollStateChanged(view, scrollState);
+            }
+            for (OnScrollListener l : clientListener) {
+                l.onScrollStateChanged(view, scrollState);
+            }
+        }
+    }
+
+    private OnScrollListener mOnScrollListener = new OnScrollListener() {
+        @Override
+        public void onScrollStateChanged(AbsListView view, int scrollState) {
+
+        }
+
+        @Override
+        public void onScroll(AbsListView view, int firstVisiblePosition, int visibleItemCount,
+                int totalItemCount) {
+            mCorrectFirstVisiblePosition = HackFirstVisiblePositionListView.super.getFirstVisiblePosition();
+        }
+    };
 }
